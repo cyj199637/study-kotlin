9c8794fa28891b3c74ff5d69b4ba2d0ee55d57b5
yoonjeong <cyj199637@gmail.com>
2022-02-01 23:45:34 +0900

    220201 - Kotlin for Java Developers: Mastermind game

---
+++ src/assignment/evaluateGuess.kt
@@ -0,0 +1,34 @@
+package assignment
+
+data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    if (secret.isNullOrEmpty())
+        throw IllegalArgumentException("secret is null or empty.")
+
+    if (guess.isNullOrEmpty())
+        throw IllegalArgumentException("guess is null or empty.")
+
+    val leftSecretLetters = secret.toMutableList()
+    val leftGuessLetters = guess.toMutableList()
+
+    var rightPosition = 0
+    var wrongPosition = 0
+
+    for ((index, letter) in guess.withIndex()) {
+        if (letter == secret[index]) {
+            rightPosition++
+            leftGuessLetters.remove(letter)
+            leftSecretLetters.remove(letter)
+        }
+    }
+
+    for (letter in leftGuessLetters) {
+        if (leftSecretLetters.contains(letter)) {
+            wrongPosition++
+            leftSecretLetters.remove(letter)
+        }
+    }
+
+    return Evaluation(rightPosition, wrongPosition)
+}
